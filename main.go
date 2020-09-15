a045b44ede54e67b9c84deee2b481f458c8a232d
igarciavalcarcel <igarciavalcarcel@gmail.com>
2020-09-15 12:17:58 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var suma int = 2 + 2
+	var resta int = 4 - 2
+	resta = 1
+	suma = 1
+	const year int = 2019
+
+	fmt.Println(suma + resta)
+	fmt.Println(year)
+}
